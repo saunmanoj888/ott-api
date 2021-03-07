6d3913e87fc2e328d44978c0ced7530654838b3f
saunmanoj888 <manoj@kiprosh.com>
2021-03-07 22:14:57 +0530

    Unit spec for User#full_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { create(:user, first_name: 'John', last_name: 'Doe') }
+
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
   it { should validate_presence_of(:first_name) }
@@ -8,4 +10,12 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:email) }
 
   it { should have_many(:ratings).dependent(:destroy) }
+
+  describe 'Instance Methods' do
+    describe '#full_name' do
+      it 'returns full name of the User' do
+        expect(user.full_name).to eq('John Doe')
+      end
+    end
+  end
 end
