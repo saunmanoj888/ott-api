ba0ded777554193495f9d775a16aedb748339603
saunmanoj888 <manoj@kiprosh.com>
2021-03-08 12:55:21 +0530

    add seed for dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create User
+
+admin = User.create(email: 'admin@example.com', password: 'qwerty', first_name: 'John', last_name: 'Doe')
+user = User.create(email: 'user@example.com', password: 'qwerty', first_name: 'User', last_name: 'Doe')
+
+# Set Admin role
+admin.add_role :admin
+
+#Create Movie
+
+movie = Movie.create(title: 'Shaolin Soccer', description: 'Comedy', release_date: '12-07-2001', budget: 123333)
+
+# Create a Person
+person1 = Person.create(name: 'Stephen Chow')
+person2 = Person.create(name: 'Zhao Wei')
+
+# add cast crew
+
+cast1 = movie.cast_crews.new(person: person1, designation: 'Actor', character: 'Mighty Stee')
+cast1.save
+
+crew1 = movie.cast_crews.new(person: person1, designation: 'Director')
+crew1.save
+
+cast2 = movie.cast_crews.new(person: person2, designation: 'Actress', character: 'Mui')
+cast2.save
