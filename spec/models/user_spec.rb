7c01ddaf1bb438f0c4f8e9fab32236fe08971c4d
saunmanoj888 <manoj@kiprosh.com>
2021-03-11 22:58:50 +0530

    Update unit spec for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user, first_name: 'John', last_name: 'Doe') }
+  let(:new_user) { build(:user) }
 
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
@@ -10,12 +11,48 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:email) }
 
   it { should have_many(:ratings).dependent(:destroy) }
+  it { should have_many(:reviews).dependent(:destroy) }
   it { should have_and_belong_to_many(:permissions) }
 
   describe 'Instance Methods' do
     describe '#full_name' do
       it 'returns full name of the User' do
         expect(user.full_name).to eq('John Doe')
+      end
+    end
+
+    describe '#can_access?' do
+      context 'When User has the applicable permission' do
+        before { create(:permission, :create_review) }
+        it 'returns true' do
+          expect(user.can_access?('can_create_review')).to eq(true)
+        end
+      end
+
+      context 'When User does not have the applicable permission' do
+        it 'returns false' do
+          expect(user.can_access?('can_delete_rating')).to eq(false)
+        end
+      end
+    end
+  end
+
+  describe 'Callbacks' do
+    describe '.set_default_user_permissions' do
+      context 'When User Sign up successfully' do
+        before do
+          Permission::List::DEFAULT_APPLICABLE.each do |permission|
+            create(:permission, name: permission)
+          end
+        end
+        it 'creates default user permissions' do
+          expect {
+            new_user.save
+          }.to change { new_user.permissions.count }.from(0).to(5)
+        end
+        it 'should not have permission to delete ratings by default' do
+          expect(user.permissions).to_not include(create(:permission, :delete_rating))
+        end
       end
     end
   end
