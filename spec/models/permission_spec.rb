797368b9c94778373a387b20091e5b3123348be9
saunmanoj888 <manoj@kiprosh.com>
2021-03-12 10:24:32 +0530

    Refactor permission_spec

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Permission, type: :model do
-  let(:create_review_permission) { Permission.find_by(name: 'can_create_review') }
-
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
   it { should have_and_belong_to_many(:users) }
@@ -11,14 +9,14 @@ RSpec.describe Permission, type: :model do
     describe '.named' do
       before { create(:permission, :create_review) }
       it 'returns permission with the given name' do
-        expect(Permission.named('can_create_review')).to include(create_review_permission)
+        expect(Permission.named('can_create_review')).to include(Permission.find_by(name: 'can_create_review'))
       end
     end
 
     describe '.non_admin_permissions' do
       before { create_admin_applicable_permissions }
       it 'includes only non admin permissions' do
-        expect(Permission.non_admin_permissions).to include(create_review_permission)
+        expect(Permission.non_admin_permissions.pluck(:name)).to eq(Permission::List::NON_ADMIN_APPLICABLE)
         expect(Permission.non_admin_permissions.count).to eq(5)
       end
       it 'does not includes permission to delete rating' do
